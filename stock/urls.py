0b003c2e4c6919c69cdf486c7a5025abab28edf9
Shubham Jain <shubham1900jain@gmail.com>
2020-11-28 13:05:35 +0530

    uploaded for hosting

--- stock/urls.py
+++ stock/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.static import serve
+from django.conf.urls import url
 from django.urls import path,include
 
 urlpatterns = [
     path('',include('price.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
-    path('price/',include('price.urls'))
+    path('price/',include('price.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
   
 ]
