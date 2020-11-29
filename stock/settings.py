1e1dbfabe048eeb3dd5b5ec5ccabaf6a910bb065
Shubham Jain <shubham1900jain@gmail.com>
2020-11-29 09:37:18 +0530

    second day

--- stock/settings.py
+++ stock/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!%1r+2(k2)6dm@h_klqqk@@tjdr%_+7xc8v*y9r*&$$+kvdx^7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://stockware.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- stock/urls.py
+++ stock/urls.py
@@ -17,13 +17,16 @@ from django.contrib import admin
 from django.views.static import serve
 from django.conf.urls import url
 from django.urls import path,include
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('',include('price.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('price/',include('price.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
   
 ]
